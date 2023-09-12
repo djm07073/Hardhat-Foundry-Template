2ebecb4b580eede9e635564f4c2a5dc7c1a96400
suha jin <89185836+djm07073@users.noreply.github.com>
2023-09-12 13:27:44 +0900

    🥳 Templete

---
+++ hardhat.config.ts
@@ -0,0 +1,77 @@
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import fs from "fs";
+import "hardhat-preprocessor";
+const importToml = require("import-toml");
+const foundryConfig = importToml.sync("foundry.toml");
+
+export default {
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  solidity: {
+    version: foundryConfig.profile.default.solc_version,
+    settings: {
+      viaIR: foundryConfig.profile.default.via_ir,
+      optimizer: {
+        enabled: true,
+        runs: foundryConfig.profile.default.optimizer_runs,
+      },
+      metadata: {
+        // do not include the metadata hash, since this is machine dependent
+        // and we want all generated code to be deterministic
+        // https://docs.soliditylang.org/en/v0.8.20/metadata.html
+        bytecodeHash: "none",
+      },
+    },
+  },
+  preprocess: {
+    eachLine: (hre) => ({
+      transform: (line: string) => {
+        if (line.match(/^\s*import /i)) {
+          for (const [from, to] of getRemappings()) {
+            if (line.includes(from)) {
+              line = line.replace(from, to);
+              break;
+            }
+          }
+        }
+        return line;
+      },
+    }),
+  },
+  paths: {
+    sources: "./src",
+    cache: "./cache_hardhat",
+  },
+};
+function getRemappings() {
+  return fs
+    .readFileSync("remappings.txt", "utf8")
+    .split("\n")
+    .filter(Boolean) // remove empty lines
+    .map((line) => line.trim().split("="));
+}
